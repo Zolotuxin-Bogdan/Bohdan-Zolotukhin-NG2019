12fbb87356130e83ee886f8a3030e45f590ffc0f
Zolotukhin_Bogdan_KB18 <b.zolotuxin@gmail.com>
2019-07-28 14:15:19 +0300

    Реалізовано Завдання 7

---
+++ Lection_1/Task_7/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int razmer,razUp,razDown = 0;
+    cout << "VVedite razmer elochki: ";
+    cin >> razmer;
+    razDown = razUp = razmer;
+    for (int i = 0;i<razmer;i++)
+    {
+        for(int i = 1;i<=razUp;i++)
+        {
+        if ((i>=razDown) && (i<=razUp))
+        cout << "*";
+        else
+        cout << " ";
+        }
+    cout << endl;
+    razDown--;
+    razUp++;
+    }
+    for (int i=1;i<razmer;i++)
+        cout << " ";
+    cout << "*";
+    return 0;
+}
