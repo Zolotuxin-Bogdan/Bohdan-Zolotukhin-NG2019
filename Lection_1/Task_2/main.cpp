0f12ca8bb3ef40a6a76993bf3e29c9d76ff0c964
Zolotukhin_Bogdan_KB18 <b.zolotuxin@gmail.com>
2019-07-28 11:19:49 +0300

    Реалізовано завдання 2

---
+++ Lection_1/Task_2/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL,"ukr");
+    float z;
+    cout << "Vvedite vashy zarplaty: ";
+    cin >> z;
+    if (z>999)
+    {
+        if (z>999999)
+            cout << "Ti millioner!";
+        if (z<1000000)
+            cout << "Horosho";
+    }
+    if (z<1000)
+    cout << "Rabotai bolshe";
+    cout << "...no ti molodec\n";
+    system("pause");
+    return 0;
+}
