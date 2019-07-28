4f28ea8d9b0baddb30996eaab341b4671cb81d13
Zolotukhin_Bogdan_KB18 <b.zolotuxin@gmail.com>
2019-07-28 11:12:32 +0300

    Реалізовано Завдання 1

---
+++ Lection_1/Task_1/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float a,b = 0;
+    cout << "Vvedite chislo 1: ";
+    cin >> a;
+    cout << "Vvedite chislo 2: ";
+    cin >> b;
+    cout << "Resultat: " << a+b << endl;
+    system("pause");
+    return 0;
+}
