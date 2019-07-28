9dd4f78b66bad66b1e4f7194c29975d90677a84d
Zolotukhin_Bogdan_KB18 <b.zolotuxin@gmail.com>
2019-07-28 11:36:03 +0300

    Реалізовано Завдання 3

---
+++ Lection_1/Task_3/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float first_val,second_val = 0;
+    int operatorr = 0;
+    cout << "Vvedite pervoe chislo: ";
+    cin >> first_val;
+    cout << "Vvedite vtoroe chislo: ";
+    cin >> second_val;
+    cout << "Vvedite operator( 1.\'+\' 2.\'-\' 3.\'/\' 4.\'*\'):";
+    cin >> operatorr;
+    if (operatorr==1)
+    cout << "Resultat: " << first_val + second_val << endl;
+    if (operatorr==2)
+    cout << "Resultat: " << first_val - second_val << endl;
+    if (operatorr==3)
+    cout << "Resultat: " << first_val / second_val << endl;
+    if (operatorr==4)
+    cout << "Resultat: " << first_val * second_val << endl;
+    if ((operatorr!=1) && (operatorr!=2) && (operatorr!=3) && (operatorr!=4))
+    cout << "Error! Vveden nepravilnyi operator...\n\n";
+    system("pause");
+    return 0;
+}
