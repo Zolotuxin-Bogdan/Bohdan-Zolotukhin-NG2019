9a8030753cce58e06a7000c83ae9254a67252f8f
Zolotukhin_Bogdan_KB18 <b.zolotuxin@gmail.com>
2019-07-30 11:19:45 +0300

    Code readability has been improved.

--- Lection_1/Task_3/main.cpp
+++ Lection_1/Task_3/main.cpp
@@ -6,25 +6,35 @@ int main()
 {
     float first_val,second_val = 0;
     int operatorr = 0;
+
     cout << "Vvedite pervoe chislo: ";
     cin >> first_val;
     cout << "Vvedite vtoroe chislo: ";
     cin >> second_val;
     cout << "Vvedite operator( 1.\'+\' 2.\'-\' 3.\'/\' 4.\'*\'):";
     cin >> operatorr;
-    if (operatorr==1)
-    cout << "Resultat: " << first_val + second_val << endl;
-    if (operatorr==2)
-    cout << "Resultat: " << first_val - second_val << endl;
-    if (operatorr==3)
-        if (second_val!=0)
-    cout << "Resultat: " << first_val / second_val << endl;
-        else
-    cout << "Error! Delenie na 0 nevozmojno...\n\n";
-    if (operatorr==4)
-    cout << "Resultat: " << first_val * second_val << endl;
-    if ((operatorr>4) || (operatorr<1))
-    cout << "Error! Vveden nepravilnyi operator...\n\n";
+        if (operatorr==1)
+        {
+            cout << "Resultat: " << first_val + second_val << endl;
+        }
+        if (operatorr==2)
+        {
+            cout << "Resultat: " << first_val - second_val << endl;
+        }
+        if (operatorr==3)
+        {
+            if (second_val!=0)
+                cout << "Resultat: " << first_val / second_val << endl;
+            else
+                cout << "Error! Delenie na 0 nevozmojno...\n\n";
+        }
+        if (operatorr==4)
+        {
+            cout << "Resultat: " << first_val * second_val << endl;
+        }
+        if ((operatorr>4) || (operatorr<1))
+        {
+            cout << "Error! Vveden nepravilnyi operator...\n\n";
+        }
     return 0;
-
 }
