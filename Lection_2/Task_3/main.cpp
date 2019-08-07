477452bc2aaf5b44fd9af77bac364432fcb798c1
Zolotukhin_Bogdan_KB18 <b.zolotuxin@gmail.com>
2019-08-07 16:40:19 +0300

    Completed Task_3

---
+++ Lection_2/Task_3/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int mass[20]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+    int numb=0;
+    do
+    {
+        cin >> mass[numb];
+        numb++;
+    } while (numb<20 && mass[numb-1]!=0);
+
+
+    int max_number=mass[0];
+    for(int i=0;i<20;i++)
+    {
+        if (mass[i] > max_number)
+            max_number = mass[i];
+    }
+
+    for(int ii=0;ii<20;ii++)
+    {
+        if(mass[ii]!=0)
+        {
+        for(int i=1;i<=(max_number-mass[ii])/2;i++)
+            cout << " ";
+        for(int i=1;i<=mass[ii];i++)
+            cout << "*";
+        cout << endl;
+        }
+    }
+    return 0;
+}
