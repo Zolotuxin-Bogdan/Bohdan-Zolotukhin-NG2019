870e31bebdce5424e292774b3263f47eb061990a
Zolotukhin_Bogdan_KB18 <b.zolotuxin@gmail.com>
2019-08-09 09:24:19 +0300

    Completed Task_4 without changing the initial cycle

--- Lection_2/Task_4/main.cpp
+++ Lection_2/Task_4/main.cpp
@@ -6,26 +6,26 @@ int main()
 {
     int zvezdi[5];
     int kol_vo=0;
+    int minuss=0;
     for(int i=0;i<5;i++)
     {
         cout << "Zvezdochek v " << i+1 << "-om stolbike: ";
         cin >> zvezdi[i];
         kol_vo+=zvezdi[i];
     }
-
    while (kol_vo>0)
    {
        for(int i=0;i<5;i++)
        {
-           if(zvezdi[i]>0)
+           if(zvezdi[i]-minuss>0)
            {
                 cout << "*";
-                zvezdi[i]--;
                 kol_vo--;
            }
            else
                 cout << " ";
        }
+        minuss++;
        cout << endl;
    }
     return 0;
