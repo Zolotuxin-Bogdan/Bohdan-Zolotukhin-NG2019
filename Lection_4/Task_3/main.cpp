338ec5b0e520e6b3153b0ec89e265b12dd1d57ff
BOGDAN_ZOLOTUKHIN_KB18 <b.zolotuxin@gmail.com>
2019-08-25 15:09:33 +0300

    completed Task_3

---
+++ Lection_4/Task_3/main.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+
+using namespace std;
+
+void showAllCards(float* carts)
+{
+    for(int i=0;i<10;i++)
+    {
+        cout << carts[i] << " ";
+    }
+}
+
+void addMoney(float* carts, int nom, float kolvo)
+{
+   carts[nom-1]+=kolvo;
+}
+
+void summa(float* carts)
+{
+    float sum = 0;
+    for(int i=0;i<10;i++)
+    {
+        sum+=carts[i];
+    }
+    cout << "\nSumma: "<< sum <<"\n\n";
+}
+
+int askNomer()
+{
+    int nomer;
+    cout << "Vvedi nomer kartochki: ";
+    cin >> nomer;
+    return nomer;
+}
+
+bool checkValidNomer(int nom)
+{
+    if (nom>=1 && nom<=10)
+    {
+        return true;
+    }
+    else
+    {
+        cout << "Error! Takoi kartu net\n\n";
+        return false;
+    }
+}
+
+int askKolvo()
+{
+    float kolvo;
+    cout << "Skolko polozhit: ";
+    cin >> kolvo;
+    return kolvo;
+}
+
+int main()
+{
+    float cards[10]={0,0,0,0,0,0,0,0,0,0};
+    int nomer=0;
+    float dengi=0;
+
+    while (1)
+    {
+        nomer=askNomer();
+        if (checkValidNomer(nomer)==true)
+        {
+            dengi=askKolvo();
+            addMoney(cards, nomer, dengi);
+            showAllCards(cards);
+            summa(cards);
+        }
+    }
+    return 0;
+}
